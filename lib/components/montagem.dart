93806dca8219cc5c0005d0d6364b14a214fb4504
alefbispo <Blacksabbath21@>
2022-06-01 23:20:51 -0300

    criando classe montagem

---
+++ lib/components/montagem.dart
@@ -0,0 +1,12 @@
+class Montagem {
+  final String nomeCliente;
+  final String endereco;
+  final String referencia;
+  final List produtos;
+
+  Montagem(
+      {required this.nomeCliente,
+      required this.endereco,
+      required this.referencia,
+      required this.produtos});
+}
