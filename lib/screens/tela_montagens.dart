9731dbe075736988719506db5525a7d449801db7
alefbispo <Blacksabbath21@>
2022-06-01 23:29:48 -0300

    preparanso proximos passos

---
+++ lib/screens/tela_montagens.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TelaMontagens extends StatelessWidget {
+  const TelaMontagens({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: ((context, index) {
+      return Text('data');
+    }));
+  }
+}
